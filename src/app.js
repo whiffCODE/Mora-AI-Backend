2b3d2b0043a5edf52cd80c84fd2c8ba24efcb0b5
Subhadip Samanta <160585009+whiffCODE@users.noreply.github.com>
2026-03-29 08:51:11 +0530

    Day 1: Backend Setup(Express server, MongoDB connection, project structure initialized)

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Mora AI Backend Running 🚀");
+});
+
+export default app;
